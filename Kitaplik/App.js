1357b3bb9d7c92d006436b85ff36f7ff312e52b8
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-06-07 16:18:30 +0300

    first commit

---
+++ Kitaplik/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Login from './src/pages/Login/Login';
+import Welcome from './src/pages/Welcome/Welcome';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}} >
+        <Stack.Screen name='WelcomeScreen' component={Welcome} />
+        <Stack.Screen name='LoginScreen' component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+export default App
---
+++ Kitaplik/assets/colors.js
@@ -0,0 +1,5 @@
+const colors = {
+    darkgreen:'#051505'
+}
+
+export default colors
