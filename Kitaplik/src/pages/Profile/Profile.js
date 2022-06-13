30807fe34920d19a46ef798135bc8232095fc70d
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-06-13 12:54:09 +0300

    profile page

--- Kitaplik/src/pages/Profile/Profile.js
+++ Kitaplik/src/pages/Profile/Profile.js
@@ -1,10 +1,12 @@
 import React from "react";
-import {View,Text} from 'react-native'
-
+import { View, Text, Image } from 'react-native'
+import styles from './Profile.style'
+import auth from '@react-native-firebase/auth'
 const Profile = () => {
-    return(
-        <View>
-            <Text>Profile Screen</Text>
+    return (
+        <View style={styles.container} >
+            <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/149/149071.png' }} style={styles.image} />
+            
         </View>
     )
 }
--- Kitaplik/src/pages/Profile/Profile.style.js
+++ Kitaplik/src/pages/Profile/Profile.style.js
@@ -1,7 +1,17 @@
 import { StyleSheet } from "react-native";
+import colors from "../../../assets/colors";
 
 export default StyleSheet.create({
     container:{
-        
+      flex:1,
+      backgroundColor:colors.darkwhite,
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    image:{
+        width:100,
+        height:100,
+        backgroundColor:'red',
+        borderRadius:60,
     }
 })
