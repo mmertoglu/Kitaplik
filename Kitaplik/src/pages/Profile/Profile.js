74dd356cad3c2c1a7c28038d1907c12ceade491e
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-06-13 14:41:22 +0300

    profile page style

--- Kitaplik/src/pages/Profile/Profile.js
+++ Kitaplik/src/pages/Profile/Profile.js
@@ -1,12 +1,22 @@
-import React from "react";
-import { View, Text, Image } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import styles from './Profile.style'
 import auth from '@react-native-firebase/auth'
+import database from '@react-native-firebase/database'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 const Profile = () => {
+    const currentUser = auth().currentUser.email.split('@',1).toString();
+    const [image,setImage] = useState('https://cdn-icons-png.flaticon.com/512/149/149071.png')
     return (
         <View style={styles.container} >
-            <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/149/149071.png' }} style={styles.image} />
-            
+            <View style={styles.header_container} ></View>
+            <Image source={{ uri: image }} style={styles.image} />
+            <View style={styles.icon_container} >
+            <MaterialIcons name="add-a-photo" size={30} color='black' />
+            </View>
+            <TouchableOpacity style={styles.edit_profile_container} >
+                <Text style={styles.edit_profile_text} >Edit Profile</Text>
+            </TouchableOpacity>
         </View>
     )
 }
--- Kitaplik/src/pages/Profile/Profile.style.js
+++ Kitaplik/src/pages/Profile/Profile.style.js
@@ -5,13 +5,39 @@ export default StyleSheet.create({
     container:{
       flex:1,
       backgroundColor:colors.darkwhite,
-      alignItems:'center',
-      justifyContent:'center'
+   
+  
     },
     image:{
         width:100,
         height:100,
         backgroundColor:'red',
         borderRadius:60,
+        position:'absolute',
+        top:70,
+        left:20,
+        borderWidth:2,
+        borderColor:'white'
+    },
+    header_container:{
+      width:'100%',
+      height:120,
+      backgroundColor:'tomato'
+    },
+    edit_profile_container:{
+      marginTop:20,
+      alignSelf:'flex-end',
+      marginRight:30,
+      borderWidth:1,
+      borderRadius:10,
+      borderColor:'black',
+      padding:8
+    },
+    edit_profile_text:{
+      color:'black',
+      fontSize:14
+    },
+    icon_container:{
+      position:'absolute'
     }
 })
